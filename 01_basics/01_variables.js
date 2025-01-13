fbc67b678408312ce838438839a244039e86f7f4
Basit-2 <abdulbasitbangash444@gmail.com>
2025-01-13 19:16:33 +0000

    let const done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123567
+let accountEmail = "b@gmail.com"
+var accountPassword= "23423"
+accountCity = "kt"
+
+console.log(accountId)
+
+//accountId = 5 constant cant change
+
+accountEmail= "ab@gmail.com"
+accountPassword=" abcd"
+accountCity= " peshawer"
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
+
