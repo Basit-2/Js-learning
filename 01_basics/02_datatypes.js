70dbce1d7f78ed9fdb32fea7ed63c5790489de83
Basit-2 <abdulbasitbangash444@gmail.com>
2025-01-13 19:43:50 +0000

    data type beg

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+ // alert () we are using node js not browser
+
+ console.log(3+3)
+
+ console.log("ab")
+
+ let name= "abb"
+ let age = 22
+ let islogedIn = true
+
+ //number
+ //bigint
+ //string = ""
+ //boolean = true/false
+ //null = standalone value
+ //undefined
+ //symbol = unique
+
+ //object
+
+ console.log(typeof "ab")
+ console.log(typeof null)
+ console.log(typeof age)
+ console.log(typeof undefined)
+ 
+ 
